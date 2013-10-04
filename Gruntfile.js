65a8becf47bb31d7045a53fef95399423a54c862
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2013-10-04 19:47:09 -0400

    Adding nuget hook to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         meta: {
             banner:
                 '/*'+
@@ -54,6 +55,36 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('nuget', 'Register NuGet-RxJSDOM', function () {
+        var done = this.async();
+
+        //invoke nuget.exe
+        grunt.util.spawn({
+            cmd: ".nuget/nuget.exe",
+            args: [
+                //specify the .nuspec file
+                "pack",
+                "nuget/RxJS-Bridges-HTML/RxJS-Bridges-HTML.nuspec",
+
+                //specify where we want the package to be created
+                "-OutputDirectory",
+                "nuget",
+     
+                //override the version with whatever is currently defined in package.json
+                "-Version",
+                grunt.config.get("pkg").version
+            ]
+        }, function (error, result) {
+            if (error) {
+                grunt.log.error(error);
+            } else {
+                grunt.log.write(result);
+            }
+
+            done();
+        });     
+    });
 
     // Default task(s).
     grunt.registerTask('default', ['concat:basic', 'uglify:basic', 'qunit']);
