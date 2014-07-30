f9b408344a991a4c99180090adaf60e58522c1de
Sergi Mansilla <sergi.mansilla@gmail.com>
2014-07-30 21:48:38 +0000

    Fix for static JSONP callbacks

--- tests/tests.ajax.js
+++ tests/tests.ajax.js
@@ -1,4 +1,4 @@
-var xhr, requests
+var xhr, requests;
 
 module('Ajax Tests', {
 	setup: function () {
@@ -590,4 +590,59 @@ test('getJSON failure', function () {
 	);
 
 	requests[0].respond(500, { 'Content-Type': 'application/json' }, 'error');
+});
+
+asyncTest('jsonpRequest with jsonp callback success', function () {
+	window.testCallback = function(observer, data) {
+		data[0].correct = true;
+		observer.onNext(data);
+		observer.onCompleted();
+	};
+	
+	var fakeScript = "data:text/javascript;base64," + btoa(
+		'testCallback([{ "id": 123 }])'
+	);
+	
+	var source = Rx.DOM.Request.jsonpRequest({
+		url: fakeScript,
+		jsonpCallback: 'testCallback'
+	});
+
+	source.subscribe(
+		function (x) {
+			equal(123, x[0].id);
+			equal(true, x[0].correct);
+		},
+		function (e) { 
+			ok(false); 
+		},
+		function () {
+			ok(true);
+			QUnit.start();
+		}
+	);
+});
+
+asyncTest('jsonpRequest without jsonp callback success', function () {
+	var fakeScript = "data:text/javascript;base64," + btoa(
+		'testCallback([{ "id": 123 }])'
+	);
+
+	var source = Rx.DOM.Request.jsonpRequest({
+		url: fakeScript,
+		jsonpCallback: 'testCallback'
+	});
+
+	source.subscribe(
+		function (x) {
+			equal(123, x[0].id);
+		},
+		function (e) { 
+			ok(false); 
+		},
+		function () {
+			ok(true);
+			QUnit.start();
+		}
+	);
 });
