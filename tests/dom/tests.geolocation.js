40defd5b76ba8cf8c2a6c5b2fb76f53898cf0b7f
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-10-07 22:03:11 -0700

    Adding tests for geolocation

--- tests/dom/tests.geolocation.js
+++ tests/dom/tests.geolocation.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   /* jshint undef: true, unused: true */
-  /* globals QUnit, test, Rx, window */
+  /* globals QUnit, test, Rx, equal, window */
   var original;
 
   QUnit.module('Geolocation', {
@@ -22,9 +22,25 @@
     },
     getCurrentPosition: function (success, error) {
       if (this._errorData) {
-        return error(this._errorData);
+        error(this._errorData);
+      } else {
+        success(this._successData);
       }
-      return success(this._successData);
+    },
+    watchPosition: function (success, error) {
+      this._watchSuccess = success;
+      this._watchError = error;
+      return 42;
+    },
+    clearWatch: function (id) {
+      this._clearWatchId = id;
+    },
+    triggerWatchPosition: function (successData, errorData) {
+      if (errorData) {
+        this._watchError(errorData);
+      } else {
+        this._watchSuccess(successData);
+      }
     }
   };
 
@@ -34,7 +50,7 @@
       onError = Rx.ReactiveTest.onError,
       onCompleted = Rx.ReactiveTest.onCompleted;
 
-  test('watchPosition success', function () {
+  test('getCurrentPosition success', function () {
     window.navigator.geolocation = mockGeolocation;
     mockGeolocation.initGetCurrentPosition(42);
 
@@ -55,7 +71,7 @@
     );
   });
 
-  test('watchPosition error', function () {
+  test('getCurrentPosition error', function () {
     var error = new Error();
     window.navigator.geolocation = mockGeolocation;
     mockGeolocation.initGetCurrentPosition(null, error);
@@ -74,6 +90,61 @@
     results.messages.assertEqual(
       onError(200, error)
     );
+  });
+
+  test('watchPosition success', function () {
+    window.navigator.geolocation = mockGeolocation;
+
+    var scheduler = new TestScheduler();
+
+    var source, subscription;
+    var results = scheduler.createObserver();
+
+    scheduler.scheduleAbsolute(null, 100, function () { source = DOM.geolocation.watchPosition(); });
+    scheduler.scheduleAbsolute(null, 200, function () { subscription = source.subscribe(results); });
+    scheduler.scheduleAbsolute(null, 1000, function () { subscription.dispose(); });
+
+    scheduler.scheduleAbsolute(null, 300, function () { mockGeolocation.triggerWatchPosition(42); });
+    scheduler.scheduleAbsolute(null, 400, function () { mockGeolocation.triggerWatchPosition(56); });
+    scheduler.scheduleAbsolute(null, 500, function () { mockGeolocation.triggerWatchPosition(78); });
+
+    scheduler.start();
+
+    results.messages.assertEqual(
+      onNext(300, 42),
+      onNext(400, 56),
+      onNext(500, 78)
+    );
+
+    equal(mockGeolocation._clearWatchId, 42);
+  });
+
+  test('watchPosition error', function () {
+    var error = new Error();
+    window.navigator.geolocation = mockGeolocation;
+
+    var scheduler = new TestScheduler();
+
+    var source, subscription;
+    var results = scheduler.createObserver();
+
+    scheduler.scheduleAbsolute(null, 100, function () { source = DOM.geolocation.watchPosition(); });
+    scheduler.scheduleAbsolute(null, 200, function () { subscription = source.subscribe(results); });
+    scheduler.scheduleAbsolute(null, 1000, function () { subscription.dispose(); });
+
+    scheduler.scheduleAbsolute(null, 300, function () { mockGeolocation.triggerWatchPosition(42); });
+    scheduler.scheduleAbsolute(null, 400, function () { mockGeolocation.triggerWatchPosition(56); });
+    scheduler.scheduleAbsolute(null, 500, function () { mockGeolocation.triggerWatchPosition(null, error); });
+
+    scheduler.start();
+
+    results.messages.assertEqual(
+      onNext(300, 42),
+      onNext(400, 56),
+      onError(500, error)
+    );
+
+    equal(mockGeolocation._clearWatchId, 42);
   });
 
 }());
